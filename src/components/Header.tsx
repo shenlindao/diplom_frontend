5a91dd6b6a7790e716ea6a9bcb208c8380ac4013
Artem <shenlin@yandex.ru>
2024-11-01 22:39:41 +0300

    header first fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,72 @@
 import React from "react";
-import { Flex, Menu } from "antd";
+import type { MenuProps } from 'antd';
+import { Dropdown, Flex, Menu } from "antd";
 import {
   HomeOutlined,
   UserOutlined,
   ContactsOutlined,
 } from "@ant-design/icons";
 
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        2nd menu item
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        3rd menu item
+      </a>
+    ),
+  },
+];
+
 const Header: React.FC = () => {
   return (
     <Flex className="header" justify="center">
-      <Flex className="wrap" justify="space-between" align="stretch" style={{ height: "100%"}}>
+      <Flex
+        className="wrap"
+        justify="space-between"
+        align="stretch"
+        style={{ height: "100%" }}
+      >
         <div className="header__logo" />
-        <Menu className="header__menu" theme="light" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<HomeOutlined />}>
-            Главная
-          </Menu.Item>
+        <Menu
+          className="header__menu"
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+        >
+          <Dropdown menu={{ items }} placement="bottom" arrow>
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              Главная
+            </Menu.Item>
+          </Dropdown>
           <Menu.Item key="2" icon={<UserOutlined />}>
             О нас
           </Menu.Item>
